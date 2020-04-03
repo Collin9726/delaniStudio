872fce63799ae09a18620ab07371ea4e52795e40
collin9726 <owino.collin@gmail.com>
2020-04-03 20:59:22 +0300

    add hover effects on portfolio section

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    //service icons toggle
     $(".design").click(function(){
         $("#design_content_show").toggle();
         $("#design_icon_show").toggle();
@@ -11,4 +12,23 @@ $(document).ready(function(){
         $("#p-mgt_content_show").toggle();
         $("#p-mgt_icon_show").toggle();
     });
+    //portfolio hover effects
+    hoverEffect("1");
+    hoverEffect("2");
+    hoverEffect("3");
+    hoverEffect("4");
+    hoverEffect("5");
+    hoverEffect("6");
+    hoverEffect("7");
+    hoverEffect("8");
 });
+
+function hoverEffect(y){
+    $(".work"+y).hover(function(){
+        $(".title-"+y).show();
+        $(".dark-bg"+y).show();
+    },function(){
+        $(".title-"+y).hide();
+        $(".dark-bg"+y).hide();
+    });
+}
